c9939008a7a2b3dfec62543905cf655f8dd95bf6
Sebastian Baltes <research@sbaltes.com>
2018-10-13 19:27:24 +0200

    Get previous and next day

--- functions.js
+++ functions.js
@@ -17,11 +17,28 @@ function readCSV(postId,  callback) {
     });
 }
 
-function extractDateString(date) {
-    return date.toISOString().split(".000Z")[0].split("T")[0];
-}
-
 function extractDateAndTimeString(date) {
     return date.toISOString().split(".000Z")[0].replace("T", " ");
 }
 
+function extractDateString(date) {
+    return date.toISOString().split(".000Z")[0].split("T")[0];
+}
+
+function getBeginningOfPreviousDay(date) {
+    var previousDay = new Date(extractDateString(date) + "T12:00:00Z");
+    previousDay.setDate(previousDay.getDate()-1); // see https://stackoverflow.com/a/5960713
+    previousDay.setUTCHours(0, 0, 1);
+    return previousDay;
+}
+
+function getMiddleOfDay(date) {
+    return new Date(extractDateString(date) + "T12:00:00Z");
+}
+
+function getEndOfNextDay(date) {
+    var nextDay = new Date(extractDateString(date) + "T12:00:00Z");
+    nextDay.setDate(nextDay.getDate()+1); // see https://stackoverflow.com/a/5960713
+    nextDay.setUTCHours(23, 59, 59);
+    return nextDay;
+}
